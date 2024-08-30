cf33eae595154e103c42b9f5c148ab683badb9ba
Julien Sulpis <julien.sulpis@gmail.com>
2024-08-30 23:03:08 +0200

    handle all screen sizes for the responsive preview

--- src/components/MultiDeviceCodeEditor/ResponsivePreview/Modal.tsx
+++ src/components/MultiDeviceCodeEditor/ResponsivePreview/Modal.tsx
@@ -5,13 +5,46 @@ import interact from "interactjs";
 import { PortalSSR } from "./PortalSSR";
 import styles from "./styles.module.scss";
 
-const previewScale = 0.5;
-const previewResolution = { width: 2560, height: 1440 };
-
 export function Modal({ id, children }: PropsWithChildren<{ id: string }>) {
-	const modal = useRef(null);
+	const modal = useRef<HTMLDivElement>(null);
 	const resolutionDisplay = useRef<HTMLElement>(null);
 	const preview = useRef<HTMLDivElement>(null);
+	const overlay = useRef<HTMLDivElement>(null);
+
+	const previewResolution = useRef({ width: 2560, height: 1440 });
+	const previewScale = useRef(1);
+
+	useEffect(() => {
+		/**
+		 * Adjust the scale of the modal depending on the screen resolution
+		 */
+		function updateScale() {
+			if (!overlay.current || !modal.current || !preview.current) {
+				return;
+			}
+			const initialWidth = Math.min(0.8 * window.innerWidth, 2500);
+			previewScale.current = initialWidth / 2560;
+			overlay.current.style.fontSize = `${previewScale.current}rem`;
+
+			Object.assign(modal.current.style, {
+				width: `${previewResolution.current.width * previewScale.current}px`,
+				height: `${previewResolution.current.height * previewScale.current}px`,
+			});
+
+			Object.assign(preview.current.style, {
+				width: `${100 / previewScale.current}%`,
+				height: `${100 / previewScale.current}%`,
+				transform: `scale(${previewScale.current})`,
+			});
+		}
+
+		updateScale();
+
+		window.addEventListener("resize", updateScale);
+		return () => {
+			window.removeEventListener("resize", updateScale);
+		};
+	}, []);
 
 	useEffect(() => {
 		MicroModal.init({ awaitCloseAnimation: true });
@@ -24,16 +57,20 @@ export function Modal({ id, children }: PropsWithChildren<{ id: string }>) {
 			edges: { left: true, right: true },
 			listeners: {
 				move: function (event) {
-					const currentWidth = +event.target.style.width.replace("px", "");
-					let newWidth = currentWidth + 2 * event.deltaRect.width;
-					newWidth = Math.max(newWidth, 320 * previewScale);
-					newWidth = Math.min(newWidth, 2800 * previewScale);
+					const currentModalWidth = +event.target.style.width.replace("px", "");
+					let newModalWidth = currentModalWidth + 2 * event.deltaRect.width;
+					newModalWidth = Math.max(newModalWidth, 320 * previewScale.current);
+					newModalWidth = Math.min(newModalWidth, 2800 * previewScale.current);
 
-					event.target.style.width = `${newWidth}px`;
+					const newDisplayWidth = Math.floor(
+						newModalWidth / previewScale.current
+					);
+					previewResolution.current.width = newDisplayWidth;
+
+					event.target.style.width = `${newModalWidth}px`;
 
 					if (resolutionDisplay.current) {
-						const displayWidth = Math.floor(newWidth / previewScale);
-						resolutionDisplay.current.innerHTML = `${displayWidth} x ${1440}`;
+						resolutionDisplay.current.innerHTML = `${newDisplayWidth} x ${1440}`;
 					}
 				},
 			},
@@ -43,17 +80,20 @@ export function Modal({ id, children }: PropsWithChildren<{ id: string }>) {
 	return (
 		<PortalSSR>
 			<div className={styles.modal} id={id} aria-hidden="true">
-				<div className={styles.overlay} tabIndex={-1}>
+				<div ref={overlay} className={styles.overlay} tabIndex={-1}>
+					<button
+						className={styles.close}
+						aria-label="Fermer la preview responsive"
+						data-micromodal-close
+					>
+						✕
+					</button>
 					<div
 						ref={modal}
 						className={styles.container}
 						role="dialog"
 						aria-modal="true"
 						aria-label="preview Responsive"
-						style={{
-							width: previewResolution.width * previewScale,
-							height: previewResolution.height * previewScale,
-						}}
 						onPointerDown={() => {
 							// prevent interference of the iframe during the resize
 							preview.current!.style.pointerEvents = "none";
@@ -63,26 +103,12 @@ export function Modal({ id, children }: PropsWithChildren<{ id: string }>) {
 							preview.current!.style.pointerEvents = "auto";
 						}}
 					>
-						<div
-							ref={preview}
-							className={styles.content}
-							style={{
-								height: `${100 / previewScale}%`,
-								width: `${100 / previewScale}%`,
-								transform: `scale(${previewScale})`,
-								transformOrigin: "top left",
-							}}
-						>
+						<span className={styles.resolution} ref={resolutionDisplay}>
+							{`${previewResolution.current.width} x ${previewResolution.current.height}`}
+						</span>
+						<div ref={preview} className={styles.content}>
 							{children}
 						</div>
-						<span className={styles.resolution} ref={resolutionDisplay}>
-							{`${previewResolution.width} x ${previewResolution.height}`}
-						</span>
-						<button
-							className={styles.close}
-							aria-label="Fermer la preview responsive"
-							data-micromodal-close
-						></button>
 					</div>
 				</div>
 			</div>

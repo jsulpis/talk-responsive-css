9600450f0e6e3320e0848cfcfb165886c353d5b9
Théo Gianella <theo.gianella@proton.me>
2024-09-19 18:38:30 +0200

    5th demo

---
+++ src/slides/1-demos/documentation/index.tsx
@@ -0,0 +1,16 @@
+import html from "./index.html?raw";
+import styles from "./styles.css?raw";
+import responsive from "./responsive.css?raw";
+import { MultiDeviceCodeEditor } from "../../../components/MultiDeviceCodeEditor";
+
+export function Documentation() {
+	return (
+		<MultiDeviceCodeEditor
+			files={{
+				"/responsive.css": responsive,
+				"/styles.css": styles,
+				"/index.html": html,
+			}}
+		/>
+	);
+}

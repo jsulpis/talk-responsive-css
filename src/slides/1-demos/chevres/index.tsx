22e921ec0f7161f4fb23f8df29b22c37da32444e
Julien Sulpis <julien.sulpis@gmail.com>
2024-09-23 14:12:16 +0200

    feat: add content platform demo + initial/final toggle

---
+++ src/slides/1-demos/chevres/index.tsx
@@ -0,0 +1,18 @@
+import html from "./index.html?raw";
+import styles from "./styles.css?raw";
+import responsive from "./responsive.css?raw";
+import responsiveFinal from "./responsive-final.css?raw";
+import { MultiDeviceCodeEditor } from "../../../components/MultiDeviceCodeEditor";
+
+export function Chevres() {
+	return (
+		<MultiDeviceCodeEditor
+			files={{
+				"/responsive.css": responsive,
+				"/responsive-final.css": responsiveFinal,
+				"/styles.css": styles,
+				"/index.html": html,
+			}}
+		/>
+	);
+}

5ebdf9fdfc18db462426f13931a2235e0baf33b3
Julien Sulpis <julien.sulpis@gmail.com>
2024-09-25 23:21:47 +0200

    feat: add final version of the gallery

--- src/slides/1-demos/gallery/index.tsx
+++ src/slides/1-demos/gallery/index.tsx
@@ -1,6 +1,7 @@
 import html from "./index.html?raw";
 import styles from "./styles.css?raw";
 import responsive from "./responsive.css?raw";
+import responsiveFinal from "./responsive-final.css?raw";
 import { MultiDeviceCodeEditor } from "../../../components/MultiDeviceCodeEditor";
 
 export function Gallery() {
@@ -8,6 +9,7 @@ export function Gallery() {
 		<MultiDeviceCodeEditor
 			files={{
 				"/responsive.css": responsive,
+				"/responsive-final.css": responsiveFinal,
 				"/styles.css": styles,
 				"/index.html": html,
 			}}

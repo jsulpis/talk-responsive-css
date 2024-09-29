7522da97b33b5a1daf39f126dcb16a06d92e51fa
Julien Sulpis <julien.sulpis@gmail.com>
2024-09-29 18:49:21 +0200

    feat: add slides after the text card

---
+++ src/slides/1-demos/text-card/demo/index.tsx
@@ -0,0 +1,29 @@
+import html from "./index.html?raw";
+import styles from "./styles.css?raw";
+import responsive from "./responsive.css?raw";
+import responsiveFinal from "./responsive-final.css?raw";
+import { MultiDeviceCodeEditor } from "../../../../components/MultiDeviceCodeEditor";
+
+export function TextCardDemo() {
+	return (
+		<MultiDeviceCodeEditor
+			files={{
+				"/responsive.css": responsive,
+				"/styles.css": styles,
+				"/index.html": html,
+			}}
+		/>
+	);
+}
+
+export function TextCardDemoFinal() {
+	return (
+		<MultiDeviceCodeEditor
+			files={{
+				"/responsive.css": responsiveFinal,
+				"/styles.css": styles,
+				"/index.html": html,
+			}}
+		/>
+	);
+}

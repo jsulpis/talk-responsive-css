5e8badc99ae6cb3fcb40915566ceda5cb0377dc7
Théo Gianella <122352170+TGianella@users.noreply.github.com>
2024-10-03 20:31:36 +0200

    Demo intro slide (#8)
    * card slide
    
    * mockups slide
    
    * feat: add intro slides
    
    ---------
    
    Co-authored-by: Julien Sulpis <julien.sulpis@gmail.com>

---
+++ src/slides/1-demos/empty/demo/index.tsx
@@ -0,0 +1,13 @@
+import { MultiDeviceCodeEditor } from "../../../../components/MultiDeviceCodeEditor";
+
+export function EmptyPageDemo() {
+	return (
+		<MultiDeviceCodeEditor
+			files={{
+				"/responsive.css": "",
+				"/styles.css": "",
+				"/index.html": "",
+			}}
+		/>
+	);
+}

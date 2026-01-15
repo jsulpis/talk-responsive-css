4ba9ae17b912f6a1ae0f55461a87ab944c1af02a
Julien Sulpis <code@jsulpis.dev>
2026-01-15 15:38:04 +0100

    feat: add events data

---
+++ src/events.ts
@@ -0,0 +1,20 @@
+const events = {
+  devoxx: {
+    introImg: "/images/intro-devoxx.webp",
+    outroImg: "/images/outro-devoxx.webp",
+  },
+  mixit: {
+    introImg: "/images/intro-mixit.webp",
+    outroImg: "/images/outro-mixit.webp",
+  },
+  rivieraDev: {
+    introImg: "/images/intro/riviera.png",
+    outroImg: "/images/outro-riviera.png",
+  },
+  snowcamp: {
+    introImg: "/images/intro/snowcamp.webp",
+    outroImg: "/images/outro-snowcamp.webp",
+  },
+};
+
+export const currentEvent = events.snowcamp;
